4af351886fbff5402f11d209e0596c1929751c94
Dev Patel <devpatel2556@gmail.com>
2022-11-06 14:05:58 -0500

    project create

---
+++ backend/AdjacencyList.py
@@ -0,0 +1,16 @@
+class AdjacencyList: 
+
+    def __init__(self) -> None:
+        self.adjacency_list = {}
+
+    def create_adjacency_list(self): 
+        print("CREATE ADJACENCY LIST HERE")
+
+    def load_adjacency_list(self): 
+        print("LOAD ADJACENCY LIST HERE")
+
+    def dijkstra_algorithm(self): 
+        print("PERFORM DIJKSTRA ALGORITHM HERE")
+
+    def second_algorithm(self): 
+        print("PERFORM SECOND ALGORITHM HERE (TBD)")
