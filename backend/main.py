10727d8d28a04ed4786890a028b6a918173167dc
BaronSwango <baronswango30@gmail.com>
2022-11-25 01:31:08 -0500

    Update gitignore

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,8 @@ import AdjacencyList
 list = AdjacencyList.AdjacencyList()
 list.create_adjacency_list()
 
-
-dj = list.dijkstra_algorithm(4202402640, 42472954, False, 0)
+dj = list.dijkstra_algorithm(39076461, 597414803, False, 0)
 print(dj)
 
-ast = list.dijkstra_algorithm(4202402640, 42472954, True, 1)
+ast = list.dijkstra_algorithm(39076461, 597414803, True, 1)
 print(ast)
