ea808ca2c81e88cf78999de81eebe98acbc2d0ad
BaronSwango <baronswango30@gmail.com>
2022-12-05 21:19:41 -0500

    Installed TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,124 +1,320 @@
-import './App.css';
-import { MapContainer, TileLayer, ZoomControl, GeoJSON } from 'react-leaflet'
-import cities from "./cities/cities"
-import { useEffect, useState } from 'react';
-import MarkerPopup from './components/MarkerPopup';
-import SideBar from './components/SideBar';
+import "./App.css";
+import { MapContainer, TileLayer, ZoomControl, GeoJSON } from "react-leaflet";
+import cities from "./cities/cities";
+import { useEffect, useState } from "react";
+import MarkerPopup from "./components/MarkerPopup";
+import SideBar from "./components/SideBar";
 
 const App = () => {
+  const [markers, setMarkers] = useState({});
+  const [viewMarkers, setviewMarkers] = useState([
+    "tourism",
+    "parks",
+    "entertainment",
+    "museums",
+    "housing",
+    "restaurants",
+  ]);
 
-	const [markers, setMarkers] = useState({})
-	const [viewMarkers, setviewMarkers] = useState(["tourism", "parks", "entertainment", "museums", "housing", "restaurants"])
-	
-	const [nodes, setNodes] = useState([])
-	const [route, setRoute] = useState(undefined)
-	const [data, setData] = useState({})
-	
-	const [geoLayer, setgeoLayer] = useState(1)
-	const [city, setCity] = useState("New York")
-	const [map, setMap] = useState(null);
+  const [nodes, setNodes] = useState([]);
+  const [route, setRoute] = useState(undefined);
+  const [data, setData] = useState({});
 
-	// Called when city is changed to load Adjacency List
-	async function changeCity(city) {
-		const response = await fetch(`/api/city/${city}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": 'application/json'
-			}
-		})
+  const [geoLayer, setgeoLayer] = useState(1);
+  const [city, setCity] = useState("New York");
+  const [map, setMap] = useState(null);
 
-		return response
-	}
+  // Called when city is changed to load Adjacency List
+  async function changeCity(city) {
+    const response = await fetch(`/api/city/${city}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-	useEffect(() => { 
+    return response;
+  }
 
-		// Change city
-		const response = changeCity(cities[city]["abrev"])
-		response.then((response) => response.json()).then((data) => {
-			console.log(`CITY CHANGED TO ${data["city"]}`)
-		})
+  useEffect(() => {
+    // Change city
+    const response = changeCity(cities[city]["abrev"]);
+    response
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(`CITY CHANGED TO ${data["city"]}`);
+      });
 
-		// Set markers
-		let tempMarkers = {}
-		for (let key in cities[city]["locations"]) {
-			let tempData = []
-			for (let val in cities[city]["locations"][key]) {
-				let info = {
-					name: cities[city]["locations"][key][val]["name"],
-					address: cities[city]["locations"][key][val]["address"],
-					coordinates: cities[city]["locations"][key][val]["coordinates"],
-					node: cities[city]["locations"][key][val]["node"],
-					feature: key,
-					icon: undefined
-				}
+    // Set markers
+    let tempMarkers = {};
+    for (let key in cities[city]["locations"]) {
+      let tempData = [];
+      for (let val in cities[city]["locations"][key]) {
+        let info = {
+          name: cities[city]["locations"][key][val]["name"],
+          address: cities[city]["locations"][key][val]["address"],
+          coordinates: cities[city]["locations"][key][val]["coordinates"],
+          node: cities[city]["locations"][key][val]["node"],
+          feature: key,
+          icon: undefined,
+        };
 
-				tempData.push(info)
-			}
+        tempData.push(info);
+      }
 
-			tempMarkers[key] = tempData
-		}
+      tempMarkers[key] = tempData;
+    }
 
-		setMarkers(tempMarkers)
-	}, [city])
+    setMarkers(tempMarkers);
+  }, [city]);
 
-	// Called to get path between two or more nodes
-	async function getPath(data) {
-		const response = await fetch("/api/dijkstra", {
-			method: "POST",
-			headers: {
-				"Content-Type": 'application/json'
-			},
-			body: JSON.stringify(data)
-		})
+  // Called to get path between two or more nodes
+  async function getPath(data) {
+    const response = await fetch("/api/dijkstra", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-		return response
-	}
+    return response;
+  }
 
-	useEffect(() => {
-		if (nodes.length < 2) {
-			setData({})
-			setRoute(undefined)
-			return
-		}
+  useEffect(() => {
+    if (nodes.length < 2) {
+      setData({});
+      setRoute(undefined);
+      return;
+    }
 
-		// When length of route is > 2, find shortest path between nodes
-		const response = getPath({nodes: nodes})
+    // When length of route is > 2, find shortest path between nodes
+    const response = getPath({ nodes: nodes });
 
-		// Set response data to resepective states
-		response.then((response) => response.json()).then((data) => {
-			setData(data["data"])
-			setgeoLayer((current) => current + 1)
-			setRoute(data["GeoJSON"])
-		})
+    // Set response data to resepective states
+    response
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data["data"]);
+        setgeoLayer((current) => current + 1);
+        setRoute(data["GeoJSON"]);
+      });
+  }, [nodes]);
 
-	}, [nodes])
-	
-	// Go to different city 
-	const fly=(city)=>{
-		setCity(city)
+  // Go to different city
+  const fly = (city) => {
+    setCity(city);
 
-		// Reset State
-		setNodes([])
-		setRoute(undefined)
-		setData({})
+    // Reset State
+    setNodes([]);
+    setRoute(undefined);
+    setData({});
 
-		map.flyTo(cities[city]["coordinates"], 13, {duration: 3})
-	}
-	
-	return (
-		<div>
-			<SideBar viewMarkers={viewMarkers} setviewMarkers={setviewMarkers}></SideBar>
-			<MapContainer center={cities[city]["coordinates"]} zoom={13} minZoom={11} zoomControl={false} scrollWheelZoom={true} ref={setMap}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				{viewMarkers ? viewMarkers.map((feature) =>
-					markers[feature] ? markers[feature].map((marker) => 
-						<MarkerPopup marker={marker} nodes={nodes} setNodes={setNodes}></MarkerPopup>): null) : null}
-				{route ? <GeoJSON key={geoLayer} data={route["features"]}></GeoJSON> : null}
-			</MapContainer>
-		</div>
-	);
-}
+    map.flyTo(cities[city]["coordinates"], 13, { duration: 3 });
+  };
+
+  return (
+    <div>
+      <button
+        className="goto"
+        onClick={() => fly("Seattle")}
+        type="button"
+        data-bs-target="#exampleModal"
+      >
+        Go To
+      </button>
+
+      <SideBar
+        viewMarkers={viewMarkers}
+        setviewMarkers={setviewMarkers}
+      ></SideBar>
+      <div className="RightSideBar">
+        <aside class="w-64" aria-label="Sidebar">
+          <div class="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
+            <ul class="space-y-2">
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                    <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                  </svg>
+                  <span class="ml-3">Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Kanban</span>
+                  <span class="inline-flex justify-center items-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                    Pro
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                    <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Inbox</span>
+                  <span class="inline-flex justify-center items-center p-3 ml-3 w-3 h-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
+                    3
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </div>
+      <div class="z-0">
+        <MapContainer
+          center={cities[city]["coordinates"]}
+          zoom={13}
+          minZoom={11}
+          zoomControl={false}
+          scrollWheelZoom={true}
+          ref={setMap}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {viewMarkers
+            ? viewMarkers.map((feature) =>
+                markers[feature]
+                  ? markers[feature].map((marker) => (
+                      <MarkerPopup
+                        marker={marker}
+                        nodes={nodes}
+                        setNodes={setNodes}
+                      ></MarkerPopup>
+                    ))
+                  : null
+              )
+            : null}
+          {route ? (
+            <GeoJSON
+              key={geoLayer}
+              data={route["features"]}
+              color="blue"
+            ></GeoJSON>
+          ) : null}
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
 export default App;
