fd63e014511bb19ad1cb5d2f4da6623af9d4f176
BaronSwango <baronswango30@gmail.com>
2022-12-06 12:42:42 -0500

    UI Tweaks

--- src/components/Data.js
+++ src/components/Data.js
@@ -9,12 +9,12 @@ const Data = ({ dijkstra_data, a_star_data }) => {
                     <tr className='row'>
                         <th className='header-col'></th>
                         <th className='header-row'>Dijkstra</th>
-                        <th className='header-row'>A*</th>
+                        <th className='header-row'>A* Search</th>
                     </tr>
                     <tr className='row'>
                         <th className='header-col'>Distance Traveled</th>
-                        <td className='row-text'>{`${dijkstra_data["distance"][0]} KM`}<br></br>{`${dijkstra_data["distance"][1]} M`}</td>
-                        <td className='row-text'>{`${a_star_data["distance"][0]} KM`}<br></br>{`${a_star_data["distance"][1]} M`}</td>
+                        <td className='row-text'>{`${parseFloat(dijkstra_data["distance"][0]).toFixed(2)} km`}<br></br>{`${parseFloat(dijkstra_data["distance"][1]).toFixed(2)} mi`}</td>
+                        <td className='row-text'>{`${parseFloat(a_star_data["distance"][0]).toFixed(2)} km`}<br></br>{`${parseFloat(a_star_data["distance"][1]).toFixed(2)} mi`}</td>
                     </tr>
                     <tr className='row'>
                         <th className='header-col'>Nodes Visited</th>
@@ -23,8 +23,8 @@ const Data = ({ dijkstra_data, a_star_data }) => {
                     </tr>
                     <tr className='row'>
                         <th className='header-col'>Execution Time</th>
-                        <td className='row-text'>{dijkstra_data["exec_time"]}</td>
-                        <td className='row-text'>{a_star_data["exec_time"]}</td>
+                        <td className='row-text'>{`${parseFloat(dijkstra_data["exec_time"]).toFixed(1)} ms`}</td>
+                        <td className='row-text'>{`${parseFloat(a_star_data["exec_time"]).toFixed(1)} ms`}</td>
                     </tr>
                 </tbody>
             </table>
